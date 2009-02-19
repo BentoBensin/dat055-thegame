effa8a4cd328e831cbc48a80d3ef49d321fcb516
josef86 <josef86@eddc2970-f73d-11dd-88c7-29a3b14d5316>
2009-02-19 15:55:51 +0000

    use this instead of strings inside the game.. import main.strings; strings.North i.e.

---
+++ src/main/strings.java
@@ -0,0 +1,23 @@
+package main;
+
+import java.lang.Enum;
+/**
+ * Every string in the whole application
+ * @file strings.java
+ * @version 0.1
+ * @author Josef Johansson
+ *
+ */
+public final class strings {
+	public static final String North = "north";
+	public static final String West = "west";
+	public static final String South = "south";
+	public static final String East = "east";
+	public static final String Attack = "attack";
+	public static final String Move = "move";
+	//todo
+	//public static final EnumSet<String> Directions = new Enum(North,1);
+	
+	public strings(){}
+	 
+}
