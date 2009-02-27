0c6602eba9ce4f6dd56d7f4c3a58bb2eff802f69
josef86 <josef86@eddc2970-f73d-11dd-88c7-29a3b14d5316>
2009-02-27 06:11:54 +0000

    refaktorering av gamecharacter

--- dude/src/main/Main.java
+++ dude/src/main/Main.java
@@ -1,14 +1,8 @@
 package main;
 
 import game.Engine;
-import game.IDGen;
-import game.Player;
-import gamecharacter.Warrior;
 import gui.Gui;
 
-import java.awt.Point;
-import java.util.ArrayList;
-import java.util.HashMap;
 /**
  * @file Main.java
  * @version 0.2
@@ -27,17 +21,7 @@ public class Main
     }
     public static void main(String [] args )
     {
-    	/*Engine e;
-    	Gui gui;
-    	e = new Engine();*/
     	new Gui();
-    	/*Player p = new Player( IDGen.generateID() , new Point( 500,250 ), 100, "player",new Warrior(), e);
-    	gui.setPlayer(p);
-    	e.addClient( p);
-    	p.startThread();
-    	
-    	e.createEnemies(3);*/
-    	
     }
     
     public void init() {
